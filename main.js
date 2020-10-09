f5196daac261900e62dfab7e31f87c4c6bd4b2ba
Moralist Festus <festusoluwatobi294@gmail.com>
2020-10-09 19:23:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
-// getUser("nabendu82");
+// getUser("MoralistFestus");
 
 async function getUser(username) {
     const resp = await fetch(API_URL + username);
