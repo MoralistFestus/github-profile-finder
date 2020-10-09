9153af017f621c20e4875d3555b96ef26ae9017e
Moralist Festus <festusoluwatobi294@gmail.com>
2020-10-09 20:14:31 +0100

    update main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-const API_URL = "https://api.github.com/users/";
+const API_URL = "https://api.github.com/users/"; // Github API
 
+// Get DOM html attribute 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
-// getUser("MoralistFestus");
+// getUser("moralistfestus");
 
+// fetch user from API_URL github API 
 async function getUser(username) {
     const resp = await fetch(API_URL + username);
     const respData = await resp.json();
@@ -13,12 +15,14 @@ async function getUser(username) {
     getRepos(username);
 }
 
+// Fetch all user's Repos
 async function getRepos(username) {
   const resp = await fetch(API_URL + username + "/repos");
   const respData = await resp.json();
   addReposToCard(respData);
 }
 
+// Add user profile to a rendered html card attribute
 function addReposToCard(repos) {
   const reposEl = document.getElementById("repos");
   repos.forEach((repo) => {
@@ -31,6 +35,7 @@ function addReposToCard(repos) {
       });
 }
 
+// User card
 function createUserCard(user) {
   const cardHTML = `
       <div class="card">
@@ -41,11 +46,17 @@ function createUserCard(user) {
               <h2>${user.name}</h2>
               <p>${user.bio}</p>
               <ul class="info">
-                  <li><strong>Followers :</strong>${user.followers}</li>
-                  <li><strong>Following :</strong>${user.following}</li>
-                  <li><strong>Repos :</strong>${user.public_repos}</li>
-                  <li><strong>Twitter :</strong> ${user.twitter_username}</li>
-                  <li><strong>Location :</strong>${user.location}</li>
+                  <li><strong>Followers: </strong>${user.followers}</li>
+                  <li><strong>Following: </strong>${user.following}</li>
+                  <li><strong>Repos: </strong>${user.public_repos}</li>
+                  <li><strong>Twitter: </strong> ${user.twitter_username}</li>
+                  <li><strong>Starred: </strong>${user.starred_url}</li>
+                  <li><strong>PublicGists: </strong>${user.public_gists}</li>
+                  <li><strong>Organization: </strong>${user.organizations_url}</li>
+                  <li><strong>Company: </strong>${user.company}</li>
+                  <li><strong>Location: </strong>${user.location}</li>
+                  <li><strong>CreatedAt: </strong>${user.created_at}</li>
+
               </ul>
               <div id="repos"></div>
           </div>
@@ -55,6 +66,7 @@ function createUserCard(user) {
   main.innerHTML = cardHTML;
 }
 
+// Prevent page from reloading or refreshing after submitting form
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const user = search.value;
